4344bd909f132f4447b26144cf79f7286b6082fe
rogerioreismuniz <rmuniz.dev@gmail.com>
2021-12-07 20:01:57 -0300

    evento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { HomeComponent } from './paginas/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
